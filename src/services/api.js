88fe86d3391632e53f9c95e7038751a56805a36f
devsfabio <fabiio403@gmail.com>
2025-01-23 19:17:37 -0300

    Deploy inicial

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'devburgerfb.netlify.app',
+  baseURL: 'http://localhost:3001',
 });
 
 api.interceptors.request.use((config) => {
